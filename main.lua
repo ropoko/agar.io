efb946f9d98b4d453e639d5e5da6764b18c23885
ropoko <rodrigostramantinoli@gmail.com>
2022-08-25 18:35:14 -0300

    feat: wip camera following player

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
+require('./camera')
+
 WINDOW_SETTINGS = {
 	width = 800,
 	height = 600
 }
 
 function love.load()
+	camera:setBounds(0, 0, WINDOW_SETTINGS.width, WINDOW_SETTINGS.height)
 	BACKGROUND = love.graphics.newImage('assets/background.jpg')
 
 	PLAYER = {
@@ -22,20 +25,45 @@ function love.update(dt)
 	local x_distance = mousex - PLAYER.x
 	local y_distance = mousey - PLAYER.y
 
-	PLAYER.x = PLAYER.x + (x_distance * 0.05)
-	PLAYER.y = PLAYER.y + (y_distance * 0.05)
+	local speed = 0.05
+
+	PLAYER.x = PLAYER.x + (x_distance * speed)
+	PLAYER.y = PLAYER.y + (y_distance * speed)
+
+	local screen_center_x = WINDOW_SETTINGS.width / 2
+	local screen_center_y = WINDOW_SETTINGS.height / 2
+
+	camera:setPosition((screen_center_x - PLAYER.x), (screen_center_y - PLAYER.y))
+
+	-- if PLAYER.x < screen_center_x then
+	-- 	camera:setPosition((screen_center_x - PLAYER.x), (screen_center_y - PLAYER.y))
+	-- end
+	-- if PLAYER.x > mousex then
+	-- 	camera:setPosition(((WINDOW_SETTINGS.width / 2) - PLAYER.x), ((WINDOW_SETTINGS.height / 2) - PLAYER.y))
+	-- end
+
+	-- if PLAYER.y < mousey then
+	-- 	camera:setPosition(((WINDOW_SETTINGS.width / 2) - PLAYER.x), ((WINDOW_SETTINGS.height / 2) - PLAYER.y))
+	-- end
+	-- if PLAYER.x > mousey then
+	-- 	camera:setPosition(((WINDOW_SETTINGS.width / 2) - PLAYER.x), ((WINDOW_SETTINGS.height / 2) - PLAYER.y))
+	-- end
 end
 
 function love.draw()
+	camera:set()
 	for i = 0, love.graphics.getWidth() / BACKGROUND:getWidth() do
 		love.graphics.setColor(255,255,255)
 		for j = 0, love.graphics.getHeight() / BACKGROUND:getHeight() do
-				love.graphics.draw(BACKGROUND, i * BACKGROUND:getWidth(), j * BACKGROUND:getHeight())
+			love.graphics.draw(BACKGROUND, i * BACKGROUND:getWidth(), j * BACKGROUND:getHeight())
 		end
 	end
 
 	love.graphics.setColor(0,0,0)
 	love.graphics.ellipse("fill", PLAYER.x, PLAYER.y, PLAYER.width, PLAYER.height)
+	-- only for reference while configuring camera
+	love.graphics.ellipse("fill", 50, 50, PLAYER.width, PLAYER.height)
+	camera:unset()
 end
 
 function love.resize(width, height)
